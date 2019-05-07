d0a7c69c11dd57d2a9180bbfadc14e83bbf37327
Neeilan Selvalingam <neeilan@outlook.com>
2019-05-06 21:05:19 -0400

    parser skeleton

--- tools/parser/parser.go
+++ tools/parser/parser.go
@@ -2,19 +2,36 @@ package main
 
 import "fmt"
 import "diff/tools/lexer"
+import "diff/tools/token"
+import df "diff/difflib"
 
 func main() {
-	New("a + b").Parse()
+	New("cos(a+b)").Parse()
 }
 
 type Parser struct {
-	l *lexer.Lexer
+	l          *lexer.Lexer
+	curr, peek token.Token
 }
 
 func New(s string) *Parser {
-	return &Parser{l: lexer.New(s)}
+	p := &Parser{l: lexer.New(s)}
+	p.advance() // initialize curr, peek
+	p.advance()
+	return p
 }
 
-func (p *Parser) Parse() {
-	fmt.Println(p.l.NextToken())
+func (p *Parser) Parse() df.Expression {
+	e := p.parseExpression(0)
+	fmt.Println(e)
+	return e
+}
+
+func (p *Parser) advance() {
+	p.curr = p.peek
+	p.peek = p.l.NextToken()
+}
+
+func (p *Parser) parseExpression(precedence int) df.Expression {
+	return &df.Number{}
 }
